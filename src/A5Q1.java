c4bd503fb0b0b1b184490499cb5d4150a47d66c5
moraj0721 <moraj0721@HRH-IC0027319.wrdsb.ca>
2015-11-16 14:18:29 -0500

    assignment 4 completed

--- src/A5Q1.java
+++ src/A5Q1.java
@@ -21,18 +21,14 @@ public class A5Q1 {
 
         System.out.println("Please enter a word");
         String word = input.nextLine();
-
-
-
-        String Mattenenglisch = word;
-//Put ee at the end if it starts with a vowel
-
-        if (Mattenenglisch.startsWith("a")
-                || Mattenenglisch.startsWith("o")
-                || Mattenenglisch.startsWith("e")
-                || Mattenenglisch.startsWith("u")) {
-            Mattenenglisch = Mattenenglisch.substring(1);
-            Mattenenglisch = "i" + Mattenenglisch;
+        String Matten = word;
+        //Put ee at the end of the word if it starts with a vowel
+        if (Matten.startsWith("a")
+                || Matten.startsWith("o")
+                || Matten.startsWith("e")
+                || Matten.startsWith("u")) {
+            Matten = Matten.substring(1);
+            Matten = "i" + Matten;
         } else {
 
             while (!word.startsWith("a")
@@ -40,35 +36,36 @@ public class A5Q1 {
                     && !word.startsWith("i")
                     && !word.startsWith("o")
                     && !word.startsWith("u")) {
+                
 
                 if (word.equals("END")) {
-                    System.out.println("No words? C ya latr alligator.");
+                    System.out.println("Buhbye!");
                     break;
                 }
-//if word starts with a vowel
-                if (!Mattenenglisch.startsWith("a")
-                        && !Mattenenglisch.startsWith("e")
-                        && !Mattenenglisch.startsWith("o")
-                        && !Mattenenglisch.startsWith("u")
-                        && !Mattenenglisch.startsWith("i")
-                        && !Mattenenglisch.startsWith("y")) {
-                    String first = Mattenenglisch.substring(0, 1);
-                    String last = Mattenenglisch.substring(1);
-                    Mattenenglisch = last + first;
-
-                    if (Mattenenglisch.startsWith("o")
-                            || Mattenenglisch.startsWith("a")
-                            || Mattenenglisch.startsWith("e")
-                            || Mattenenglisch.startsWith("u")) {
-                        String firstLetter = Mattenenglisch.substring(0, 1);
-                        Mattenenglisch = Mattenenglisch.replaceFirst(firstLetter, "i");
-                        System.out.println("In Mattenenglisch: " + Mattenenglisch + "ee");
+                    //if the word starts with a vowel put it at the end of the word
+                if (Matten.startsWith("a")
+                        || Matten.startsWith("e")
+                        || !Matten.startsWith("o")
+                        || !Matten.startsWith("u")
+                        || !Matten.startsWith("i")
+                        || !Matten.startsWith("y")) {
+                    String first = Matten.substring(0, 1);
+                    String last = Matten.substring(1);
+                    Matten = last + first;
+            //Replace it with i
+                    if (Matten.startsWith("o")
+                            || Matten.startsWith("a")
+                            || Matten.startsWith("e")
+                            || Matten.startsWith("u")) {
+                        String firstLetter = Matten.substring(0, 1);
+                        Matten = Matten.replaceFirst(firstLetter, "i");
+                        System.out.println("In Matten: " + Matten + "ee");
                         break;
                     }
                 } else {
-                    String firstLetter = Mattenenglisch.substring(0, 1);
-                    Mattenenglisch = Mattenenglisch.replaceFirst(firstLetter, "i");
-                    System.out.println("Mattenenglisch: " + Mattenenglisch + "ee");
+                    String firstLetter = Matten.substring(0, 1);
+                    Matten = Matten.replaceFirst(firstLetter, "i");
+                    System.out.println("Matten: " + Matten + "ee");
                     break;
                 }
 
